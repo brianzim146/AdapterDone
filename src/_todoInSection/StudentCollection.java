44f506a02eb6ce9fd8ed881bb89bccae78d64c82
brianzim146 <brianzim@email.arizona.edu>
2017-03-08 16:22:15 -0700

    Finished StudentCollection

--- src/_todoInSection/StudentCollection.java
+++ src/_todoInSection/StudentCollection.java
@@ -3,6 +3,9 @@ package _todoInSection;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
+
 import model.Student;
 
 /**
@@ -16,50 +19,108 @@ import model.Student;
 // (after adding implements TableModel to the class heading).
 //
 // Note: Some TableModel methods need not be implemented.
-public class StudentCollection  {
+public class StudentCollection  implements TableModel {
 
-  private List<Student> theStudents;
+	private List<Student> theStudents;
 
-  /**
-   * The constructor for a StudentCollection. Initializes the list of
-   * students.
-   */
-  public StudentCollection() {
-    theStudents = new ArrayList<Student>();
-    setUpDefaultList();
-  }
+	/**
+	 * The constructor for a StudentCollection. Initializes the list of
+	 * students.
+	 */
+	public StudentCollection() {
+		theStudents = new ArrayList<Student>();
+		setUpDefaultList();
+	}
 
-  /**
-   * Initializes the list with a few hard-coded students
-   */
-  private void setUpDefaultList() {
-    theStudents.add(new Student("Kim", "Computer Science", 4.0, 23));
-    theStudents.add(new Student("Chris", "Computer Science", 4.0, 23));
-    theStudents.add(new Student("Devon", "Math", 2.7, 21));
-    theStudents.add(new Student("Ashley", "Biology", 3.29, 27));
-    theStudents.add(new Student("Taylor", "Economics", 3.9, 25));
-    theStudents.add(new Student("Chris", "Economics", 3.8, 19));
-    theStudents.add(new Student("Dakota", "Psychology", 1.99, 22));
-  }
- 
-  public int size() {
-    return theStudents.size();
-  }
+	/**
+	 * Initializes the list with a few hard-coded students
+	 */
+	private void setUpDefaultList() {
+		theStudents.add(new Student("Kim", "Computer Science", 4.0, 23));
+		theStudents.add(new Student("Chris", "Computer Science", 4.0, 23));
+		theStudents.add(new Student("Devon", "Math", 2.7, 21));
+		theStudents.add(new Student("Ashley", "Biology", 3.29, 27));
+		theStudents.add(new Student("Taylor", "Economics", 3.9, 25));
+		theStudents.add(new Student("Chris", "Economics", 3.8, 19));
+		theStudents.add(new Student("Dakota", "Psychology", 1.99, 22));
+	}
 
-  public void add(Student newStudent) {
-    theStudents.add(newStudent);
-  }
+	public int size() {
+		return theStudents.size();
+	}
 
-  public Student get(int index) {
-    return theStudents.get(index);
-  }
+	public void add(Student newStudent) {
+		theStudents.add(newStudent);
+	}
 
-  public Student getFirstStudentWithName(String name) {
-    for (int index = 0; index < size(); index++) {
-      if (name.equals(theStudents.get(index).getName()))
-        return theStudents.get(index);
-    }
-    return null; // not found
-  }
+	public Student get(int index) {
+		return theStudents.get(index);
+	}
+
+	public Student getFirstStudentWithName(String name) {
+		for (int index = 0; index < size(); index++) {
+			if (name.equals(theStudents.get(index).getName()))
+				return theStudents.get(index);
+		}
+		return null; // not found
+	}
+
+	@Override
+	public int getRowCount() {
+		return size();
+	}
+
+	@Override
+	public int getColumnCount() {
+		return 4;
+	}
+
+	@Override
+	public String getColumnName(int columnIndex) {
+		if (columnIndex == 0) return "Name";
+		if (columnIndex == 1) return "Major";
+		if (columnIndex == 2) return "GPA";
+		else return "Age";
+	}
+
+	@Override
+	public Class<?> getColumnClass(int columnIndex) {
+		if (columnIndex == 0) return String.class;
+		if (columnIndex == 1) return String.class;
+		if (columnIndex == 2) return Double.class;
+		else return Integer.class;
+	}
+
+	@Override
+	public boolean isCellEditable(int rowIndex, int columnIndex) {
+		return false;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		Student student = theStudents.get(rowIndex);
+		if (columnIndex == 0) {
+			return student.getName();
+		}
+		if (columnIndex == 1) {
+			return student.getMajor();
+		}
+		if (columnIndex == 2) {
+			return student.getGPA();
+		}
+		else return student.getAge();
+	}
+
+	@Override
+	public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+	}
+
+	@Override
+	public void addTableModelListener(TableModelListener l) {
+	}
+
+	@Override
+	public void removeTableModelListener(TableModelListener l) {
+	}
 
 }
